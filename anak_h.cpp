1fc99ae4d722a2663a4c83b41fc783e0299aef85
zzaain <sheriellaghzain@gmail.com>
2026-05-11 11:44:36 +0700

    Menambahkan data member dan constructor

--- anak_h.cpp
+++ anak_h.cpp
@@ -6,3 +6,13 @@
 using namespace std;
 
 class anak {
+public:
+    string nama;
+    anak(string pNama) :nama(pNama) {
+        cout << "Anak \"" << nama << "\" ada\n";
+    }
+    ~anak() {
+        cout << "Anak \"" << nama << "\" tidak ada\n";
+    }
+};
+#endif
