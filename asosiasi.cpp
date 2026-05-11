33bed3952cf4d241d5354d4fe603fd0f004a4970
zzaain <sheriellaghzain@gmail.com>
2026-05-11 12:01:03 +0700

    Deklarasi forward class dokter dan definisi class pasien

---
+++ asosiasi.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class dokter;
+class pasien { // asosiasi
+public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+    pasien(string pNama) :nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    }
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }
+    void tambahDokter(dokter*);
+    void cetakDokter();
+};
