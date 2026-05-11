459b923f91dab5637cb63eeab86abc4f64d0fd05
zzaain <sheriellaghzain@gmail.com>
2026-05-11 12:01:34 +0700

    Definisi class dokter dengan atribut dan method

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -16,3 +16,18 @@ public:
     void tambahDokter(dokter*);
     void cetakDokter();
 };
+
+class dokter {
+public:
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    dokter(string pNama) :nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+    ~dokter() {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+    void tambahPasien(pasien*);
+    void cetakPasien();
+};
