48d81880b70115a78a2c71718cab6b777d9713e8
zzaain <sheriellaghzain@gmail.com>
2026-05-11 12:07:08 +0700

    Menambahkan constructor, destructor, dan method perkenalan pada class pelajar

--- inheritance.cpp
+++ inheritance.cpp
@@ -22,3 +22,13 @@ public:
 class pelajar : public orang {
 public:
     string sekolah;
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+        cout << "pelajar dibuat\n" << endl;
+    }
+    ~pelajar() {
+        cout << "pelajar dihapus\n" << endl;
+    }
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
+    }
+};
