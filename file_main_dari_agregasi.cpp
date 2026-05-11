02380ae5a2ccbd024d0c68b8cd19338b85c92bce
zzaain <sheriellaghzain@gmail.com>
2026-05-11 11:58:19 +0700

    Menghubungkan relasi antar objek, mencetak data, dan manajemen memori (delete)

--- file_main_dari_agregasi.cpp
+++ file_main_dari_agregasi.cpp
@@ -10,3 +10,22 @@ int main() { //agregasi
     anak* varAnak1 = new anak("tono");
     anak* varAnak2 = new anak("rini");
     anak* varAnak3 = new anak("Dewi");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varibu2->tambahAnak(varAnak3);
+    varibu2->tambahAnak(varAnak1);
+
+    varIbu->cetakAnak();
+    varibu2->cetakAnak();
+
+    delete varIbu;
+    delete varibu2;
+    delete varAnak1;
+    delete varAnak2;
+    delete varAnak3;
+
+    system("pause");
+
+    return 0;
+}
