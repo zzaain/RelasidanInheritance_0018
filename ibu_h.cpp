aa7045a0186625d08f50d6839e39719d2b6b3032
zzaain <sheriellaghzain@gmail.com>
2026-05-11 11:50:04 +0700

    Menambahkan constructor, destructor, dan prototype fungsi

--- ibu_h.cpp
+++ ibu_h.cpp
@@ -6,3 +6,12 @@ class ibu {
 public:
     string nama;
     vector<anak*> daftar_anak;
+    ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+    void tambahAnak(anak*);
+    void cetakAnak();
+};
