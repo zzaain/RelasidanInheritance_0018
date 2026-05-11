60edd4f9361a26e92a071b0d6b69c806e9889e71
zzaain <sheriellaghzain@gmail.com>
2026-05-11 11:56:54 +0700

    Instansiasi objek ibu dan anak menggunakan heap (new)

--- file_main_dari_agregasi.cpp
+++ file_main_dari_agregasi.cpp
@@ -3,3 +3,10 @@ using namespace std;
 
 #include "anak.h"
 #include "ibu.h"
+
+int main() { //agregasi
+    ibu* varIbu = new ibu("dini");
+    ibu* varibu2 = new ibu("novi");
+    anak* varAnak1 = new anak("tono");
+    anak* varAnak2 = new anak("rini");
+    anak* varAnak3 = new anak("Dewi");
