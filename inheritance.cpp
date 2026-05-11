34e35df2f5d9fb7df0968b984a7a9e302ce9aeb5
zzaain <sheriellaghzain@gmail.com>
2026-05-11 12:06:09 +0700

    Menambahkan constructor, destructor, dan method jumlah pada class orang

--- inheritance.cpp
+++ inheritance.cpp
@@ -5,3 +5,16 @@ using namespace std;
 class orang {
 public:
     string nama;
+    orang(string pNama) :
+        nama(pNama) {
+        cout << "orang dibuat\n" << endl;
+    }
+    ~orang() {
+        cout << "orang dihapus\n" << endl;
+    }
+
+    int jumlah(int a, int b) {
+        return a + b;
+    }
+
+};
