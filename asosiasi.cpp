2c65eb47d630788c59659d43fb67cc625e1eee8b
zzaain <sheriellaghzain@gmail.com>
2026-05-11 12:03:54 +0700

    Menghubungkan relasi, menampilkan data, dan dealokasi memori

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -62,3 +62,20 @@ int main() {
     dokter* varDokter2 = new dokter("dr.Tono");
     pasien* varPasien1 = new pasien("Andi");
     pasien* varPasien2 = new pasien("Lia");
+
+    varDokter1->tambahPasien(varPasien1);
+    varDokter1->tambahPasien(varPasien2);
+    varDokter2->tambahPasien(varPasien1);
+
+    varDokter1->cetakPasien();
+    varDokter2->cetakPasien();
+    varPasien1->cetakDokter();
+    varPasien2->cetakDokter();
+
+    delete varPasien1;
+    delete varPasien2;
+    delete varDokter1;
+    delete varDokter2;
+
+    return 0;
+}
