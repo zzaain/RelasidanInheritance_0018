3adc43107ebf7efdabbb1485e8d6b11e34c78492
zzaain <sheriellaghzain@gmail.com>
2026-05-11 12:08:30 +0700

    Implementasi fungsi main untuk instansiasi objek dan pemanggilan method

--- inheritance.cpp
+++ inheritance.cpp
@@ -32,3 +32,11 @@ public:
         return "Hallo, nama saya " + nama + " dari sekolah " + sekolah + "\n\n";
     }
 };
+
+int main() {
+    pelajar siswa1("andi laksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+}
