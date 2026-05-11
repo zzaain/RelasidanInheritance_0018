fbe5c2123da92f830c86f0236365bd6ecdf0e5a4
zzaain <sheriellaghzain@gmail.com>
2026-05-11 11:50:42 +0700

    Implementasi fungsi tambahAnak dan cetakAnak

--- ibu_h.cpp
+++ ibu_h.cpp
@@ -15,3 +15,18 @@ public:
     void tambahAnak(anak*);
     void cetakAnak();
 };
+
+void ibu::tambahAnak(anak* pAnak) {
+    daftar_anak.push_back(pAnak);
+}
+void ibu::cetakAnak() {
+    cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
+    /*for (auto& a : daftar_anak) {
+        cout << a->nama << "\n";
+    }*/
+    for (int i = 0; i < daftar_anak.size(); i++) {
+        cout << daftar_anak[i]->nama << endl;
+    }
+    cout << endl;
+}
+#endif
